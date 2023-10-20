1262b7ac510376481be30a6603e7bbd45fd704a7
ltb <ltb@ltb.litslab.net>
2023-10-20 02:36:07 +0200

    first commit

---
+++ psyapp/accounts/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('L\'adresse e-mail est obligatoire')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    USER_TYPE_CHOICES = (
+        ('Patient', 'Patient'),
+        ('Psy', 'Psy'),
+    )
+    email = models.EmailField(unique=True)
+    nom = models.CharField(max_length=255)
+    prenom = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True, null=True)
+    pseudo = models.CharField(max_length=255)
+    date_naissance = models.DateField()
+    telephone = PhoneNumberField()
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nom', 'prenom', 'date_naissance', 'telephone', 'user_type']
+
+    def __str__(self):
+        return self.email
